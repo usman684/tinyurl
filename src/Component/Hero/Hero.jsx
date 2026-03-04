9a3df49a28de8f3aa6cdd8698da8c16ce26d6c5b
Muhammad Usman <usman.rauf.953@gmail.com>
2026-03-04 16:21:54 +0500

    url

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -146,7 +146,7 @@ const Hero = () => {
 
     <FontAwesomeIcon
       icon={faCircleExclamation}
-      className="absolute left-3 top-1/2  -translate-y-1/2 text-gray-500"
+      className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
     />
 
     <input
